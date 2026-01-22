7e82109d2dc93791c5b86caddf7888adcb13f69e
A.Ozkan <alan.ozkan@student.atlantaclassical.org>
2026-01-22 15:02:23 -0500

    outline function

--- test.c
+++ test.c
@@ -1,1 +1,9 @@
 #include <stdio.h>
+int main(){
+    void buildarray(){
+        //create and fill an array
+    }
+    void searcharray(){
+        //create int i and search each index of the array
+    }
+}
