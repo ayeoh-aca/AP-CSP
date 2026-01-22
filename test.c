90ee3c403566f0a4dd98879af0b44e24503ef3c5
A.Ozkan <alan.ozkan@student.atlantaclassical.org>
2026-01-22 15:20:31 -0500

    create buildarray

--- test.c
+++ test.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main(){
-    void buildarray(){
-        //create and fill an array
+    int [50] buildarray(){
+        //create and fill an array with even numbers 2-100
+        int array [50];
+        for (int i=0; i<50; i++){
+            array[i] = (i+1)*2;
+        }
     }
     void searcharray(){
         //create int i and search each index of the array
