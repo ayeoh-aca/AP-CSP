f18dab95c2a7226d96ae07905af265d55b0f20a5
A.Ozkan <alan.ozkan@student.atlantaclassical.org>
2026-02-11 15:20:03 -0500

    Many Python Codes wooo

---
+++ birthday_msg.py
@@ -0,0 +1,11 @@
+name = "Georgie"
+#declared a variable and called it name
+#Assigned the string Georgie to variable
+print(f"Happy Birthday to {name}")
+#this is called an f-string--curly braces names its an existing variable
+#in c this would be printf("Happy birthday to %s", name);
+#In python:
+#there is no need to compile,
+#there is no main function, 
+#there is *dynamic data typing* (auto assigns the data type)
+#there is a much bigger standard library (no need for a header)
