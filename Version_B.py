80583e32246940e9e4b436fbf3e848e125a1c52d
A.Ozkan <alan.ozkan@student.atlantaclassical.org>
2026-02-20 14:17:52 -0500

    I finsihed

--- Version_B.py
+++ Version_B.py
@@ -16,3 +16,27 @@ side = 6
 area = side * side
 print(area)
 #6
+first = "Grace"
+last = "Hopper"
+print(f"{first} {last}")
+#7
+a = 5
+b = 8
+print(a <= b)
+print(b == 10)
+#8
+has_fuel = True
+engine_check = False
+if has_fuel == True and not engine_check == False:
+    print("car can start")
+else:
+    print("car cannot start")
+#9
+val1 = True
+val2 = False
+print(val1 + val2)#number one appears because the binary value of boolean opperators is one or zero, so 1 + 0 is 1
+#10
+name = "Alice"
+blue_marbles = 12
+red_marbles = 8
+print(f"{name} has {blue_marbles + red_marbles} marbles")
