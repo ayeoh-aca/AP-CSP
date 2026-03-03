35eb1856266c326d5fc9f1552e5925ea5cbee191
A.Ozkan <alan.ozkan@student.atlantaclassical.org>
2026-03-03 14:43:32 -0500

    calling the actual function

--- fourthings.py
+++ fourthings.py
@@ -1,5 +1,6 @@
 def custom_msg():
     age = input("What's your age: ")
-    name = input("What's your name")
-    major = input("What's your major")
+    name = input("What's your name: ")
+    major = input("What's your major: ")
     print(f"Hello {name}. You're {age} years old in {major}.")
+custom_msg()
