0dc5efa9d975751c00b351b8b19c1424b8503459
A.Ozkan <alan.ozkan@student.atlantaclassical.org>
2026-03-09 15:19:25 -0400

    test

--- fourthings.py
+++ fourthings.py
@@ -3,4 +3,16 @@ def custom_msg():
     name = input("What's your name: ")
     major = input("What's your major: ")
     print(f"Hello {name}. You're {age} years old in {major}.")
-custom_msg()
+def firstletter(studentnames):
+    for name in studentnames:
+        student = studentnames[0]
+        if student == 'A':
+            student = studentwitha
+    return studentwitha
+firstletter(["Alan", "Sam", "Ethan"])
+def avg_grade(grades):
+    sum += grades
+    amount += 1
+    avg = sum/amount
+    return avg
+avg_grade([23, 24, 25, 26])
---
+++ range.py
@@ -0,0 +1,4 @@
+ap_cps = ["Sam", "Kiki", "Alan", "Tim", "Miles", "Sean", "Melanie", "Lyndi"]
+for i in range(len(ap_cps)):
+    if (i%3)==0:
+        print(ap_cps[i])
