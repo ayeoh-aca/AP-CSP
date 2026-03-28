65c0fc053fcd3d320d93416cf75bea06316217c2
A.Ozkan <alan.ozkan@student.atlantaclassical.org>
2026-03-28 19:55:30 -0400

    This took a while, so I stopped at 5 from your email's advice

---
+++ pyhomework.py
@@ -0,0 +1,82 @@
+#1.
+balance = 0
+while balance < 150:
+    amount = int(input("Insert coins (in cents): "))
+    balance = amount + balance
+    if balance < 150:
+        x = 150 - balance
+        print(f"You still need {x} cents.")
+print("Enjoy your snack!")
+change = balance - 150
+if change > 0:
+    print(f"Change owed: {change} cents")
+#2.
+goal = int(input("How many reps is your goal? "))
+reps = 0
+while reps < goal:
+    input("Press enter to log a rep: ")
+    reps = reps + 1
+    print(f"Reps done: {reps}/{goal}")
+print("Set complete! GREAT WORK.")
+#3.
+correct_pin = "4821"
+attempts = 0
+guess = ""
+while attempts < 3 and guess != correct_pin:
+    guess = input("Enter PIN: ")
+    attempts = attempts + 1
+    remain = 3 - attempts
+    if guess != correct_pin:
+        print(f"Incorrect. You have {remain} attempt(s) remaining")
+if guess == correct_pin:
+    print("Access granted!")
+else:
+    print("Account locked. Too many failed attempts")
+#4.
+songs = []
+answer = ""
+while answer != "done".lower():
+    answer = input("Add a song (or type 'done' to finish): ")
+    if answer != "done".lower():
+        songs.append(answer)
+if songs == "":
+    print("No songs added.")
+else:
+    print("Your Playlist: ")
+    for song in range(len(songs)):
+        print((song+1), (songs[song]))
+#5.
+fridge = []
+ingredient = ""
+while ingredient != "done".lower():
+    ingredient = input("Enter an ingredient (or 'done' to finish): ")
+    if ingredient != "done".lower():
+        fridge.append(ingredient)
+needed = ["eggs", "butter", "milk"]
+haveeggs = 0
+havebutter = 0
+havemilk = 0
+haveall = 0
+for item in range(len(fridge)):
+    if fridge[item] == "eggs" and haveeggs == 0:
+        print("eggs ... HAVE IT")
+        haveall = haveall +1
+        haveeggs = 1
+    if fridge[item] == "butter" and havebutter == 0:
+        print("butter ... HAVE IT")
+        haveall = haveall +1
+        havebutter = 1
+    if fridge[item] == "milk" and havemilk == 0:
+        print("milk ... HAVE IT")
+        haveall = haveall +1
+        havemilk = 1
+if haveeggs == 0:
+    print("eggs ... MISSING")
+if havebutter == 0:
+    print("butter ... MISSING")
+if havemilk == 0:
+    print("milk ... MISSING")
+if haveall == 3:
+    print("You can make scrambled eggs!")
+else:
+    print("You are missing ingredients.")
